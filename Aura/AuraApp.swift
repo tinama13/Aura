6c16d5a17562c4f221f19e39d9b5579be714dfb6
tinama13 <tinma@umich.edu>
2026-07-07 10:16:30 -0500

    Initial Commit

---
+++ Aura/AuraApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuraApp.swift
+//  Aura
+//
+//  Created by Tina Ma on 7/7/26.
+//
+
+import SwiftUI
+
+@main
+struct AuraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
