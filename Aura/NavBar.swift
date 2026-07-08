36903f7fb2b7aaf3752d629dee2ca4080e4c5ff1
tinama13 <tinma@umich.edu>
2026-07-08 10:20:40 -0500

    Added nav bar

---
+++ Aura/NavBar.swift
@@ -0,0 +1,52 @@
+//
+//  NavBar.swift
+//  Aura
+//
+//  Created by Tina Ma on 7/8/26.
+//
+
+import SwiftUI
+
+struct NavBar: View {
+    @Binding var current_tab: Tab
+    
+    var body: some View {
+        HStack {
+            ForEach(Tab.allCases, id: \.rawValue) {
+                tab in
+                
+                Spacer()
+                
+                Button (action: {
+                    current_tab = tab
+                }) {
+                    VStack {
+                        Image(systemName: tab.iconName)
+                            .font(.system(size: 24))
+                            .foregroundColor(.black)
+                            .padding(.horizontal, 20)
+                            .padding(.vertical, 8)
+                            .background(
+                                Capsule()
+                                    .fill(current_tab == tab ? Color.purple.opacity(0.15) : Color.clear)
+                            )
+                        
+                        Text(tab.rawValue)
+                            .font(.caption2)
+                            .foregroundColor(.black.opacity(0.7))
+                    }
+                }
+                
+                Spacer()
+            }
+        }
+        
+        .padding(.top, 12)
+        .padding(.bottom, 30)
+        .background(Color(red: 0.88, green: 0.93, blue: 0.96))
+    }
+}
+
+#Preview {
+    NavBar(current_tab: .constant(.home))
+}
---
+++ Aura/PresetsView.swift
@@ -0,0 +1,23 @@
+//
+//  PresetsView.swift
+//  Aura
+//
+//  Created by Tina Ma on 7/8/26.
+//
+
+import SwiftUI
+
+struct PresetsView: View {
+    var body: some View {
+        VStack {
+                    Text("Hello, Presets!")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                }
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+#Preview {
+    PresetsView()
+}
